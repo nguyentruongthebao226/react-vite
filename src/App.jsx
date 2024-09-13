243b07d22f783eb11fac62f5b7c1211d99df2f69
DESKTOP-3A7BJ1J\THINKPAD <t2gportal@gmail.com>
2024-09-13 09:43:33 +0700

    component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/learn/MyComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,12 +19,8 @@ function App() {
       </div>
       <h1>Hello World</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <MyComponent />
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,13 @@
+
+const MyComponent = () => {
+    const thebao = "brody nguyễn";
+    return (
+        <>
+            <div>{thebao}</div>
+            <div className="child" style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
+    )
+}
+
+export default MyComponent
