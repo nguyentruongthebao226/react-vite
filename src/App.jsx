766ea756683b698b8e89043f2668699c2baa665c
DESKTOP-3A7BJ1J\THINKPAD <t2gportal@gmail.com>
2024-09-18 11:43:44 +0700

    Video 48

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import reactLogo from './assets/react.svg';
 import { useState } from 'react';
 import Header from './components/layout/header';
 import Footer from './components/layout/footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
@@ -50,6 +51,7 @@ function App() {
           <img className='logo' src={reactLogo} />
         </div>
       </div>
+      <Outlet />
       <Footer />
     </>
   )
